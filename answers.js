99a494f1601438c8ff9ab5e099c0d502ee478e11
babakkhosravifar <babak.khosravifar@gmail.com>
2015-11-14 14:11:23 +0000

    answers.js exercise1

--- answers.js
+++ answers.js
@@ -1,1 +1,11 @@
 // here we start writing javascript
+var answer1 = returnFirstCharacter("Babak");
+function returnFirstCharacter(string)
+{
+    return string[0];
+}
+
+console.log(answer1);
+console.log(returnFirstCharacter("javascript"));
+console.log(returnFirstCharacter("Javascript"));
+console.log(returnFirstCharacter(""));
