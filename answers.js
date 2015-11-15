e42f37f642555c714885d413496fdf105e1e50a5
babakkhosravifar <babak.khosravifar@gmail.com>
2015-11-15 00:47:39 +0000

    answers.js exercise2

--- answers.js
+++ answers.js
@@ -1,11 +1,27 @@
 // here we start writing javascript
-var answer1 = returnFirstCharacter("Babak");
+
+
+//Ex1-------------------------------------------
 function returnFirstCharacter(string)
 {
     return string[0];
 }
 
+//var answer1 = returnFirstCharacter("Babak");
+//console.log(answer1);
+//console.log(returnFirstCharacter("javascript"));
+//console.log(returnFirstCharacter("Javascript"));
+//console.log(returnFirstCharacter(""));
+
+
+//EX2------------------------------------------
+function returnLastCharacter(string)
+{
+    return string[string.length-1];
+}
+
+var answer1 = returnLastCharacter("Babak");
 console.log(answer1);
-console.log(returnFirstCharacter("javascript"));
-console.log(returnFirstCharacter("Javascript"));
-console.log(returnFirstCharacter(""));
+console.log(returnLastCharacter("javascript"));
+console.log(returnLastCharacter("Javascript"));
+console.log(returnLastCharacter(""));
